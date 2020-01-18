a267353c466cdce21cc66476a987bcf715c1f2d0
Stephen Byrne <s.byrne6@gmail.com>
2020-01-18 12:59:10 +0000

    initial commit

---
+++ src/index.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
+
+class App extends React.Component {
+    // Not required generally. Initialises state. Overrides component
+
+    state = { lat: null, errorMessage: '' };
+
+    componentDidMount(){
+        window.navigator.geolocation.getCurrentPosition(
+            position => this.setState({ lat: position.coords.latitude }),
+                //occurs later, not until position is actually fetched       
+            err => this.setState({ errorMessage: err.message})
+        );  
+    }
+ 
+
+    // React says we have to define render
+    render() {  
+        // Conditional Rendering   
+            if (this.state.errorMessage && !this.state.lat){
+                return <div>Error: {this.state.errorMessage}</div>
+            }
+            if (!this.state.errorMessage && this.state.lat){
+                return <SeasonDisplay lat={this.state.lat} />
+            }
+            return <div>Loading...</div>  
+    }
+}
+ReactDOM.render(
+    <App />,
+    document.querySelector('#root')
+);

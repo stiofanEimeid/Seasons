9a770990cb8387b4d2c26ce94c98549148cf8f3c
Stephen Byrne <s.byrne6@gmail.com>
2020-01-18 14:00:25 +0000

    css and loading screen functionality added

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
+import Spinner from './Spinner';
 
 class App extends React.Component {
     // Not required generally. Initialises state. Overrides component
@@ -25,7 +26,7 @@ class App extends React.Component {
             if (!this.state.errorMessage && this.state.lat){
                 return <SeasonDisplay lat={this.state.lat} />
             }
-            return <div>Loading...</div>  
+            return <Spinner />;
     }
 }
 ReactDOM.render(
